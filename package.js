494d8f3a781624da88b42b60a9b33e32fb6fe954
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2014-12-16 12:43:03 +0530

    version bumped

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Use npm modules with your Meteor App",
-  version: "1.2.0",
+  version: "1.2.1",
   git: "https://github.com/meteorhacks/npm.git",
   name: "meteorhacks:npm"
 });
